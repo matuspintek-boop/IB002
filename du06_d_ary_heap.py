dfb17c50ebf36a03504f376059e60d5d4f456b8f
Matus Pintek <xpintek@nymfe04.fi.muni.cz>
2026-02-18 12:40:10 +0100

    du06 finished and mypy checked

--- du06_d_ary_heap.py
+++ du06_d_ary_heap.py
@@ -43,6 +43,18 @@ class DMinHeap:
 # Implementujte funkci is_correct, která ověří, zda je zadaná halda korektní,
 # tj. zda splňuje haldovou podmínku.
 
+def get_parent_index(arity: int, index: int) -> int:
+    """
+    vstup: ‹arity›  arita haldy; celé číslo ≥ 2
+           ‹index›  index prvku v poli reprezentujícím haldu; celé číslo ≥ 0
+    výstup: index rodiče prvku na zadaném indexu v haldě
+    časová složitost: O(1)
+    extra prostorová složitost: O(1)
+    """    
+    if index > 0:
+        return (index - 1) // arity  
+    return 0
+
 def is_correct(heap: DMinHeap) -> bool:
     """
     vstup: ‹heap› – objekt typu DMinHeap
@@ -61,12 +73,32 @@ def is_correct(heap: DMinHeap) -> bool:
     Halda DMinHeap([1, 2, 3, 4, 2, 3, 4, 4, 4, 5, 4, 5, 3], 3) není korektní.
     Halda DMinHeap([1, 2, 3, 4, 2, 3, 4, 4, 4, 5, 4, 5, 3], 2) je korektní.
     """
-    pass  # TODO
+    for i in range(len(heap.array) - 1, -1, -1):
+        parent_index = get_parent_index(heap.arity, i)
+
+        if heap.array[parent_index] > heap.array[i]:
+            return False
+    return True
 
 
 # Část 2.
 # Implementujte funkci heapify, která opraví haldu směrem dolů od zadaného
 # indexu.
+
+def find_min_son(heap: DMinHeap, i: int) -> int:
+    min: int = i
+    for son_index in range(heap.arity * i +1, heap.arity*i + heap.arity+1):
+        if son_index < len(heap.array) and heap.array[min] > heap.array[son_index]:
+            min = son_index
+    return min
+
+def swap(heap: DMinHeap, i: int , j: int) -> None:
+    value = heap.array[i]
+    heap.array[i] = heap.array[j]
+    heap.array[j] = value
+
+    return None
+
 def heapify(heap: DMinHeap, index: int) -> None:
     """
     vstup: ‹heap›   objekt typu DMinHeap
@@ -89,7 +121,11 @@ def heapify(heap: DMinHeap, index: int) -> None:
     a zavoláme-li heapify s indexem 2, pak výsledná halda může mít například
     tvar [7, 4, 0, 5, 6, 8, 9, 3, 2, 1, 2, 2].
     """
-    pass  # TODO
+    min = find_min_son(heap, index)
+    if heap.array[min] < heap.array[index]:
+        swap(heap, index, min)
+        heapify(heap, min)
+    return None
 
 
 # Část 3.
@@ -109,7 +145,11 @@ def change_arity(heap: DMinHeap, new_arity: int) -> None:
     časová složitost: O(n), kde n je počet prvků haldy
     extra prostorová složitost: O(log n)
     """
-    pass  # TODO
+    heap.arity = new_arity
+
+    for i in range(len(heap.array) - 1, -1, -1):
+        heapify(heap, i)
+    return None
 
 
 # Část 4.
@@ -130,4 +170,42 @@ def min_three(heap: DMinHeap) -> tuple[int, int, int]:
     být výstupem trojice (1, 2, 2).
     Pro vstup DMinHeap([1, 1, 1], 42) má být výstupem trojice (1, 1, 1).
     """
-    pass  # TODO
+
+    min2 = 1
+    for i in range(1, min(heap.arity+1, len(heap.array))):
+        if heap.array[min2] > heap.array[i]:
+            min2 = i
+    if min2 == 1:
+        min3 = 2
+    else:
+        min3 = 1
+
+    for i in range(1, min(heap.arity + 1, len(heap.array))):
+        if heap.array[i] < heap.array[min3] and i != min2:
+            min3 = i
+
+
+    for i in range(heap.arity*min2+1, min(heap.arity*(min2 + 1) + 1, len(heap.array))):
+        if heap.array[i] < heap.array[min3]:
+            min3 = i
+
+    return  (heap.array[0], heap.array[min2], heap.array[min3])
+
+# def test_is_correct() -> None:
+#     assert  is_correct(DMinHeap([1, 2, 3, 2, 1, 2, 3, 2], 4))
+#     assert not is_correct(DMinHeap([1, 2, 3, 2, 1, 2, 3, 2], 2))
+#     assert not is_correct(DMinHeap([1, 2, 3, 4, 2, 3, 4, 4, 4, 5, 4, 5, 3], 3))
+#     assert is_correct(DMinHeap([1, 2, 3, 4, 2, 3, 4, 4, 4, 5, 4, 5, 3], 2))
+#     assert is_correct(DMinHeap([], 3))
+#     assert is_correct(DMinHeap([1], 2))
+
+
+#     heap = DMinHeap([1, 2, 3, 2, 1, 2, 3, 2], 4)
+#     change_arity(heap, 2)
+#     assert is_correct(heap)
+
+#     assert min_three(DMinHeap([1, 2, 4, 3, 4, 5, 6, 4, 7], 2)) == (1, 2, 3)
+#     assert min_three(DMinHeap([1, 1, 1], 42)) == (1,1,1)
+#     assert min_three(DMinHeap([1, 3, 3, 2, 4, 4, 4, 5, 5, 5, 6, 6, 2], 3)) == (1, 2, 2)
+# if __name__ == "__main__":
+#     test_is_correct()
